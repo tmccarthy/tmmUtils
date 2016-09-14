6b56c109459cff6fec8f9b2acba8d6c0c039b1ef
tmccarthy <ebh042@gmail.com>
2016-09-15 07:14:28 +1000

    Fix SortedSet implementation

--- utils/src/main/scala/au/id/tmm/utilities/collection/CollectionUtils.scala
+++ utils/src/main/scala/au/id/tmm/utilities/collection/CollectionUtils.scala
@@ -1,14 +1,13 @@
 package au.id.tmm.utilities.collection
 
-import scala.collection.SortedSet
-import scala.collection.immutable.TreeSet
+import scala.collection.immutable.{SortedSet, TreeSet}
 
 object CollectionUtils {
   implicit class Crossable[X](xs: Traversable[X]) {
     def cross[Y](ys: Traversable[Y]): Traversable[(X, Y)] = for {x <- xs; y <- ys} yield (x, y)
   }
 
-  implicit class Sortable[X](sortableSet: Iterable[X]) {
-    def toSortedSet(implicit ordering: Ordering[X]): SortedSet[X] = TreeSet(sortableSet.toList :_*)(ordering)
+  implicit class Sortable[X](sortable: Iterable[X]) {
+    def toSortedSet(implicit ordering: Ordering[X]): SortedSet[X] = TreeSet(sortable.toList :_*)(ordering)
   }
 }
