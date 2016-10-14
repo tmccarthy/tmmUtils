bb51882517189b7aca27976a939cdec501cebc41
tmccarthy <ebh042@gmail.com>
2016-10-14 22:05:28 +1100

    Remove empty for Postcode

--- utils/src/main/scala/au/id/tmm/utilities/geo/australia/Postcode.scala
+++ utils/src/main/scala/au/id/tmm/utilities/geo/australia/Postcode.scala
@@ -6,6 +6,3 @@ final case class Postcode(code: String) {
   require(code.length == 4, "A postcode must have exactly 4 characters")
   require(code.toCharArray.forall(CharSets.LATIN_DIGITS.contains), "A postcode can contain only digits")
 }
-
-object Postcode {
-}
