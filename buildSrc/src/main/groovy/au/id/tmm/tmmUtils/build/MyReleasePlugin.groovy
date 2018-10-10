5b93ab0853e3e2e010730378ec6b990a44f89412
Timothy McCarthy <ebh042@gmail.com>
2018-10-10 21:29:48 +1100

    Resolve keyring file against root project

--- buildSrc/src/main/groovy/au/id/tmm/tmmUtils/build/MyReleasePlugin.groovy
+++ buildSrc/src/main/groovy/au/id/tmm/tmmUtils/build/MyReleasePlugin.groovy
@@ -31,7 +31,7 @@ final class MyReleasePlugin implements Plugin<Project> {
         target.apply(plugin: 'signing')
 
         target.ext.'signing.keyId' = property(target, 'signing.keyId', 'GNU_KEY_ID')
-        target.ext.'signing.secretKeyRingFile' = property(target, 'signing.secretKeyRingFile', 'GNU_KEY_FILE')
+        target.ext.'signing.secretKeyRingFile' = target.rootProject.file('secring.gpg').absolutePath
         target.ext.'signing.password' = property(target, 'signing.password', 'GNU_KEY_PASSWORD')
 
         target.extensions.add(MyReleasePluginExtension.class, EXTENSION_NAME, new MyReleasePluginExtension())
