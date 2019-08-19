1239c9d139d73c432854df519005907cc5f4bd26
Timothy McCarthy <ebh042@gmail.com>
2019-08-19 22:25:07 +1000

    Scalafmt

--- codec/src/main/scala/au/id/tmm/utilities/codec/binarycodecs/HexSyntax.scala
+++ codec/src/main/scala/au/id/tmm/utilities/codec/binarycodecs/HexSyntax.scala
@@ -6,14 +6,15 @@ import org.apache.commons.codec.binary.Hex
 
 trait HexSyntax {
 
-  def asHexString(bytes: Array[Byte]): String = Hex.encodeHexString(bytes)
-  def asHexString(bytes: ByteArray): String = asHexString(ByteArray.unwrapUnsafe(bytes))
+  def asHexString(bytes: Array[Byte]): String    = Hex.encodeHexString(bytes)
+  def asHexString(bytes: ByteArray): String      = asHexString(ByteArray.unwrapUnsafe(bytes))
   def asHexString(bytes: Iterable[Byte]): String = asHexString(bytes.toArray)
 
   private def decodeToBytes(string: String): Array[Byte] = Hex.decodeHex(string)
-  def parseHexOrThrow(string: String): ByteArray = ByteArray.wrapUnsafe(decodeToBytes(string))
+  def parseHexOrThrow(string: String): ByteArray         = ByteArray.wrapUnsafe(decodeToBytes(string))
   def parseHex(string: String): Either[DecoderException, ByteArray] =
-    try Right(parseHexOrThrow(string)) catch {
+    try Right(parseHexOrThrow(string))
+    catch {
       case e: DecoderException => Left(e)
     }
 
