7a2851b1b4bd00dfce9d9dc2938437444a7712ba
Timothy McCarthy <timothy.mccarthy@rea-group.com>
2020-05-12 16:50:00 +1000

    Remove dead code

--- cats/src/main/scala/au/id/tmm/utilities/cats/instances/package.scala
+++ cats/src/main/scala/au/id/tmm/utilities/cats/instances/package.scala
@@ -3,6 +3,5 @@ package au.id.tmm.utilities.cats
 package object instances {
 
   object mapOverKeys         extends MapOverKeysInstances
-  object tmmUtilsNonEmptySet extends TmmUtilsNonEmptySetInstances
 
 }
