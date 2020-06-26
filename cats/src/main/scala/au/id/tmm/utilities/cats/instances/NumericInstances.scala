809ec9417353fb9044bdffb438074654bebfcd89
Timothy McCarthy <ebh042@gmail.com>
2020-06-26 16:08:09 +1000

    Scalafmt

--- cats/src/main/scala/au/id/tmm/utilities/cats/instances/NumericInstances.scala
+++ cats/src/main/scala/au/id/tmm/utilities/cats/instances/NumericInstances.scala
@@ -24,26 +24,30 @@ trait NumericInstances {
 object NumericInstances {
 
   private[instances] class IMappedNumeric[A, B](numericA: Numeric[A])(f: A => B)(g: B => A) extends Numeric[B] {
-    @inline override def plus(x: B, y: B): B = f(numericA.plus(g(x), g(y)))
-    @inline override def minus(x: B, y: B): B = f(numericA.minus(g(x), g(y)))
-    @inline override def times(x: B, y: B): B = f(numericA.times(g(x), g(y)))
-    @inline override def negate(x: B): B = f(numericA.negate(g(x)))
-    @inline override def fromInt(x: Int): B = f(numericA.fromInt(x))
-    @inline override def toInt(x: B): Int = numericA.toInt(g(x))
-    @inline override def toLong(x: B): Long = numericA.toLong(g(x))
-    @inline override def toFloat(x: B): Float = numericA.toFloat(g(x))
-    @inline override def toDouble(x: B): Double = numericA.toDouble(g(x))
-    @inline override def compare(x: B, y: B): Int = numericA.compare(g(x), g(y))
+    @inline override def plus(x: B, y: B): B                 = f(numericA.plus(g(x), g(y)))
+    @inline override def minus(x: B, y: B): B                = f(numericA.minus(g(x), g(y)))
+    @inline override def times(x: B, y: B): B                = f(numericA.times(g(x), g(y)))
+    @inline override def negate(x: B): B                     = f(numericA.negate(g(x)))
+    @inline override def fromInt(x: Int): B                  = f(numericA.fromInt(x))
+    @inline override def toInt(x: B): Int                    = numericA.toInt(g(x))
+    @inline override def toLong(x: B): Long                  = numericA.toLong(g(x))
+    @inline override def toFloat(x: B): Float                = numericA.toFloat(g(x))
+    @inline override def toDouble(x: B): Double              = numericA.toDouble(g(x))
+    @inline override def compare(x: B, y: B): Int            = numericA.compare(g(x), g(y))
     @inline override def parseString(str: String): Option[B] = numericA.parseString(str).map(f)
   }
 
-  private[instances] class IMappedFractional[A, B](fractionalA: Fractional[A])(f: A => B)(g: B => A) extends IMappedNumeric[A, B](fractionalA)(f)(g) with Fractional[B] {
+  private[instances] class IMappedFractional[A, B](fractionalA: Fractional[A])(f: A => B)(g: B => A)
+      extends IMappedNumeric[A, B](fractionalA)(f)(g)
+      with Fractional[B] {
     @inline override def div(x: B, y: B): B = f(fractionalA.div(g(x), g(y)))
   }
 
-  private[instances] class IMappedIntegral[A, B](integralA: Integral[A])(f: A => B)(g: B => A) extends IMappedNumeric[A, B](integralA)(f)(g) with Integral[B] {
+  private[instances] class IMappedIntegral[A, B](integralA: Integral[A])(f: A => B)(g: B => A)
+      extends IMappedNumeric[A, B](integralA)(f)(g)
+      with Integral[B] {
     @inline override def quot(x: B, y: B): B = f(integralA.quot(g(x), g(y)))
-    @inline override def rem(x: B, y: B): B = f(integralA.rem(g(x), g(y)))
+    @inline override def rem(x: B, y: B): B  = f(integralA.rem(g(x), g(y)))
   }
 
 }
--- testing/cats/src/main/scala/au/id/tmm/utilities/testing/cats/instances/WrappedInstances.scala
+++ testing/cats/src/main/scala/au/id/tmm/utilities/testing/cats/instances/WrappedInstances.scala
@@ -5,17 +5,19 @@ import cats.kernel.{Eq, Hash}
 
 trait WrappedInstances extends LowPriorityWrappedInstances {
 
-  implicit def tmmUtilsHashForWrapped[A : Hash]: Hash[Wrapped[A]] = new Hash[Wrapped[A]] {
-    override def hash(x: Wrapped[A]): Int = Hash[A].hash(x.unwrap)
-    override def eqv(x: Wrapped[A], y: Wrapped[A]): Boolean = Eq[A].eqv(x.unwrap, y.unwrap)
-  }
+  implicit def tmmUtilsHashForWrapped[A : Hash]: Hash[Wrapped[A]] =
+    new Hash[Wrapped[A]] {
+      override def hash(x: Wrapped[A]): Int                   = Hash[A].hash(x.unwrap)
+      override def eqv(x: Wrapped[A], y: Wrapped[A]): Boolean = Eq[A].eqv(x.unwrap, y.unwrap)
+    }
 
 }
 
 private[instances] trait LowPriorityWrappedInstances {
 
-  implicit def tmmUtilsEqForWrapped[A : Eq]: Eq[Wrapped[A]] = new Eq[Wrapped[A]] {
-    override def eqv(x: Wrapped[A], y: Wrapped[A]): Boolean = Eq[A].eqv(x.unwrap, y.unwrap)
-  }
+  implicit def tmmUtilsEqForWrapped[A : Eq]: Eq[Wrapped[A]] =
+    new Eq[Wrapped[A]] {
+      override def eqv(x: Wrapped[A], y: Wrapped[A]): Boolean = Eq[A].eqv(x.unwrap, y.unwrap)
+    }
 
 }
