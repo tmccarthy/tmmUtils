604d36fcf008118749342e5ed9c6044f5b8e10f6
Timothy McCarthy <ebh042@gmail.com>
2021-08-04 11:39:09 +1000

    Some more type aliases

--- cats/cats-effect/src/main/scala/au/id/tmm/utilities/cats/effect/package.scala
+++ cats/cats-effect/src/main/scala/au/id/tmm/utilities/cats/effect/package.scala
@@ -1,8 +1,11 @@
 package au.id.tmm.utilities.cats
 
 import cats.effect.IO
-import cats.effect.kernel.Resource
+import cats.effect.kernel.{Ref, Resource}
+import cats.effect.std.Semaphore
 
 package object effect {
   type ResourceIO[+A] = Resource[IO, A]
+  type RefIO[A]       = Ref[IO, A]
+  type SemaphoreIO    = Semaphore[IO]
 }
